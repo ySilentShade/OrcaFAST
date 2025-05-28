a28011bda66bd82348ddf59b00e2fef89ad92f0c
Phadyl <kleuverketchum@gmail.com>
2025-05-28 05:29:24 +0000

    Entendo, nesse caso...pode voltar a parte de orçamento para ser o que era antes de você fazer essas modificações? Estava perfeito...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
     const opt = {
       margin: 0.5, filename: `orcamento_${clientNameSanitized || 'orcafast'}.pdf`,
       image: { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { useCORS: true, backgroundColor: '#18191b' }, 
+      html2canvas:  { scale: 2, useCORS: true, backgroundColor: '#18191b' }, 
       jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
     };
     html2pdf().from(element).set(opt).save();
@@ -365,3 +365,4 @@ export default function Home() {
     </div>
   );
 }
+
